9a1e3ed2012d83197428432b4848229bad25c457
lamperouge <philbohn1791@gmail.com>
2021-09-09 15:20:17 -0400

    started setting up arrays and fundamental parts of generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,7 @@
 
 // WHEN I click the button to generate a password
 // THEN I am presented with a series of prompts for password criteria
+  // Need prompt boxes to pop up allowing entrance of data that user inputs
 
 
 // WHEN prompted for password criteria
@@ -11,10 +12,12 @@
 
 // WHEN prompted for the length of the password
 // THEN I choose a length of at least 8 characters and no more than 128 characters
+  // prompt to choose password length (with parameters set for being larger than 7 and less than)
 
 
 // WHEN asked for character types to include in the password
 // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+  // Confirm windows for each of these (?)
 
 
 // WHEN I answer each prompt
@@ -48,3 +51,31 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var aZ = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var aZLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numOption = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharacters = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "}", "~"]
+
+
+function generatePassword() {
+
+  alert ("Please select from the following criteria regarding your password");
+
+  var charNum = prompt ("How many characters?");
+
+  if (charNum < 8 || charNum > 128) {
+    alert ("Password must be between 8 and 128 characters long");
+    
+  }
+
+
+
+
+
+
+
+
+
+
+}
