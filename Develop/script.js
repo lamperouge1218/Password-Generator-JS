6e164652b6cecb8cd7a3c37fa2b663e991aac3ad
lamperouge <philbohn1791@gmail.com>
2021-09-09 23:00:52 -0400

    it's done lol

--- Develop/script.js
+++ Develop/script.js
@@ -1,46 +1,3 @@
-// GIVEN I need a new, secure password
-
-
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// Need prompt boxes to pop up allowing entrance of data that user inputs
-
-
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-
-
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// prompt to choose password length (with parameters set for being larger than 7 and less than)
-
-
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// Confirm windows for each of these (?)
-
-
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// if/else statments should the choices be true or false
-
-
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// We have a finalArray that will have all of the possible choices in it provided user input
-// Link the final array to a string of appropriate length and put random array components into that string
-
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-  // Password generator --> accepts criteria for password from user --> concantenates relevant criteria choices into a finalArray
-  // --> finalArray needs to be evaluated and random items from it need to be added to a string equal to the length of charNum 
-
-
-
-
-
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -58,12 +15,13 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-var aZ = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var aZLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var aZ = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numOption = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialCharacters = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "}", "~"];
 
 var finalArray = [];
+var passwordArray = []
 
 
 function generatePassword() {
@@ -76,14 +34,16 @@ function generatePassword() {
   //Dialog box wherein user can input password desired length (no fewer than 8 characters, no more than 128)
   if (charNum < 8 || charNum > 128) {
     alert("Password must be between 8 and 128 characters long");
-    var charNum = prompt("How many characters?");
+    var charNum = prompt("How many characters?"); 
+    //Prompt will enforce password length and charNum will update based on most recent acceptible length
   }
   else {
-    console.log(charNum);
+    console.log(charNum);  
 
   }
 
-
+  //The following are the prompt boxes to select whether user wants the related criteria.
+  //Each prompt box has a confirmation box for whether user picks Ok or Cancel
   var lowCaseConfirm = confirm("Do you want lowercase letters?");
 
   if (lowCaseConfirm === true) {
@@ -92,7 +52,7 @@ function generatePassword() {
     alert("We will include lowercase letters.");
 
   }
-  else if (lowCaseConfirm === false) {
+  else {
     alert("We will not include lowercase letters.")
   }
 
@@ -105,7 +65,7 @@ function generatePassword() {
     alert("We will include uppercase letters.");
 
   }
-  else if (upCaseConfirm === false) {
+  else {
     alert("We will not include uppercase letters.")
   }
 
@@ -116,7 +76,7 @@ function generatePassword() {
     console.log(finalArray);
     alert("We will include numbers.");
   }
-  else if (numConfirm === false) {
+  else {
     alert("We will not include numbers.");
   }
 
@@ -128,28 +88,23 @@ function generatePassword() {
     alert("We will include special characters.");
 
   }
-  else if (specCharConfirm === false) {
+  else {
     alert("We will not include special characters.");
   }
 
-  console.log(charNum);
+  //Failsafe for if no criteria are chosen. Will restart the function from the beginning and wait for acceptible criteria
+  if (finalArray.length === 0) {
+    alert ("At least one criterion must be chosen!");
+    generatePassword();
+  }
 
-  
-  
+  //for statement that takes random characters from the finalArray and turns them into a final password. 
+  for (let i = 0; i < charNum; i++) {
+    passwordArray.push (finalArray[Math.floor(Math.random() * finalArray.length)]);
+  }
 
+  //displays final password string taken from the passwordArray
+  alert (passwordArray.join(""));  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  console.log(passwordArray);
 }
